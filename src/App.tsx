264b9f5dc53ba77a307969d844fb57dcd46db3d5
jinmin.kim <bmpr14@gmail.com>
2024-03-10 15:04:52 +0900

    feat: add StyleX plugin, Zustand, update deps & implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,106 @@
-import "./App.css";
+import { useState } from "react";
+import * as stylex from "@stylexjs/stylex";
+import { useBearStore } from "./store";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+const styles = stylex.create({
+  bar: (height) => ({
+    height,
+    width: 100,
+    backgroundColor: "green",
+  }),
+});
+
+function BearCounter() {
+  const bears = useBearStore((state) => state.bears);
+  return <h1>{bears} around here ...</h1>;
+}
+
+function Controls() {
+  const increasePopulation = useBearStore((state) => state.increasePopulation);
+  return <button onClick={increasePopulation}>one up</button>;
+}
 
 function App() {
+  const [height] = useState(10);
+
   return (
     <>
       <h1>_</h1>
       <input type="text" />
       <a href="">go</a>
+      <div {...stylex.props(styles.bar(height))} />
+      <BearCounter />
+      <Controls />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
   );
 }
 
